0be29f300443ae2d8710fa6cd5e8e9f9684ea9ff
Thebosscoder316 <65313804+Thebosscoder316@users.noreply.github.com>
2022-08-26 15:24:50 -0500

    Create MainLogin.py

---
+++ MainLogin.py
@@ -0,0 +1,7 @@
+UserSelectedPassword = {Password}
+LoginPassword = UserSelectedPassword
+if UserSelectedPassword == LoginPassword:
+    SystemExit(mainlogin):
+format(mainlogin = 0)
+print("System Background Check > Find " + {WindowsError(MainFind)} + " if found=not" + Error = False + "Found. Login=true;")
+Systemexit(Load)
